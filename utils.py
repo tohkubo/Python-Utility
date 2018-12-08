31949438a17c3f03b4c30f267253277392e6a15e
tohkubo <tohkubo@uci.edu>
2018-12-08 13:41:00 -0800

    Add comments

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-
-
 import os
 import re
 import csv
@@ -8,6 +6,7 @@ import shutil
 from collections import defaultdict
 from datetime import datetime as dt
 from functools import reduce
+
 
 def count(it, sorted = True):
 	'''
@@ -38,10 +37,32 @@ def humansort(lst):
 
 
 def prod(iterable):
+	'''
+	Produce the product of all values in
+	the iterable (i.e.: list).
+	'''
 	return reduce(lambda x,y : x * y, iterable)
 
 
+def fib(v):
+	'''
+	The classic recursive approach to tackling
+	the fib sequence. Cons: you end up repeating
+	many of the same computations.
+	'''
+	if v == 0:
+		return 0
+	elif v == 1:
+		return 1
+	return fib(v - 1) + fib(v - 2)
+
+
 def fib_(v):
+	'''
+	Another way to tackle the fib sequence.
+	No need for extra computations; just
+	use/access what you already have.
+	'''
 	data = [None for _ in range(v + 1)]
 	data[0] = 0
 	data[1] = 1
@@ -49,13 +70,4 @@ def fib_(v):
 	for i in range(2, v + 1):
 		data[i] = data[i - 1] + data[i - 2]
 	return data[v]
-
-
-def fib(v):
-	if v == 0:
-		return 0
-	elif v == 1:
-		return 1
-	return fib(v - 1) + fib(v - 2)
-
 
